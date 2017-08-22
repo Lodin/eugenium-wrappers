b6bf55b6fc7240ea67c7d18204d934efd1c90fe2
Vlad Rindevich <rindevich.vs@gmail.com>
2017-08-22 23:14:52 +0300

    feat: add exporting components from index file

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,2 @@
-export class Student {
-  public fullName: string;
-
-  constructor(
-    public firstName: string,
-    public middleInitial: string,
-    public lastName: string,
-  ) {
-    this.fullName = `${firstName} ${middleInitial} ${lastName}`;
-  }
-}
-
-export interface IPerson {
-  firstName: string;
-  lastName: string;
-}
-
-export function greeter(person: IPerson): string {
-  return `Hello, ${person.firstName} ${person.lastName}`;
-}
+export {Button} from './button';
+export {TextArea} from './textarea';
