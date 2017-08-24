e2adc104a5510cae93ac725f0d4f95c26809dd9f
Vlad Rindevich <rindevich.vs@gmail.com>
2017-08-24 03:33:03 +0300

    feat(basics): add attaching shadow root to Base class

--- src/basics/base.ts
+++ src/basics/base.ts
@@ -1,6 +1,12 @@
 export abstract class Base<T extends HTMLElement> extends HTMLElement {
   protected abstract _nativeElement: T;
 
+  public constructor() {
+    super();
+
+    this.attachShadow({mode: 'open'});
+  }
+
   protected setBooleanAttribute(name: string, value: boolean): void {
     if (value) {
       this.setAttribute(name, '');
